b5764cceab39dbbffd56b39db8fc866c5af3ea67
w2030 <w2030@e-mirim.hs.kr>
2022-05-24 12:20:59 +0900

    itemrepository

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -63,4 +63,59 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("상품명 or 상품상세설명 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+
+        for (Item item :
+                itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item :
+                itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPrice(10005);
+        for (Item item :
+                itemList) {
+            System.out.println(item.toString());
+        }
+    }
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");    // @Param 어노테이션으로 값 전달
+        for (Item item :
+                itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");    // @Param 어노테이션으로 값 전달
+        for (Item item :
+                itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
 }
